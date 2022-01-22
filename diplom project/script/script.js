6546dc515102621ba10f1688eb590d9cff65b894
eshchurovskaya <95284351+eshchurovskaya@users.noreply.github.com>
2022-01-22 09:59:54 +0200

    Add files via upload

--- diplom project/script/script.js
+++ diplom project/script/script.js
@@ -1,8 +1,14 @@
 $('.hiring-items > div').on('click', function(){
     $(this).children('.hiring-icon').toggleClass('active');
+    $(this).children('.hiring-town').toggleClass('toggleColor');
+    $(this).children('.hiring-position').toggleClass('toggleColor');
     $(this).parent('.hiring-items').toggleClass('toggleBg');
     $(this).toggleClass('toggleColor');
     $(this).next('p').slideToggle(300).toggleClass('toggleColor');
 })
 
-$('')
+$('.feature-sc > div').on('click', function(){
+    $(this).next('p').slideToggle(300);
+    $(this).children('.feature-customers').toggleClass('toggleBlue');
+    $(this).next('hr').toggleClass('toggleBack');
+})
