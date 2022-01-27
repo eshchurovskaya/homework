ee4401d5cc724e4ac01a783090c355c64d3cb821
eshchurovskaya <95284351+eshchurovskaya@users.noreply.github.com>
2022-01-27 14:11:43 +0200

    Add files via upload

--- diplom project/script/modal.js
+++ diplom project/script/modal.js
@@ -11,3 +11,17 @@ $('.close').on('click', function(){
 $('#modal-button').on('click', function(){
     $('.modal-overlay').fadeIn();
 })
+
+$('.modal-hiring').on('click', function(e){
+    if($(e.target).closest('.about-company').length==0){
+        $(this).fadeOut()
+    }
+})
+
+$('.plus').on('click', function(){
+    $(this).parents('.modal-hiring').fadeOut();
+})
+
+$('#modal-button_hiring').on('click', function(){
+    $('.modal-hiring').fadeIn();
+})
