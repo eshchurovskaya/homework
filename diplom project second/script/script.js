e4469e5816c4b65a31b5a276bdf0e955316bd2e9
eshchurovskaya <95284351+eshchurovskaya@users.noreply.github.com>
2022-02-09 12:21:17 +0200

    Add files via upload

--- diplom project second/script/script.js
+++ diplom project second/script/script.js
@@ -1,3 +1,14 @@
 $('.carousel').slick({
     dots: true,
+    slidesToShow: 4,
+    variableWidth: true,
+    slidesToScroll: 3,
 });
+
+$('.carousel-clients').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1.5,
+});
+
+
