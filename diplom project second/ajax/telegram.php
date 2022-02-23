c2f7a8d84dbbe51b281123661e884fa349816e80
eshchurovskaya <95284351+eshchurovskaya@users.noreply.github.com>
2022-02-23 17:22:08 +0200

    Add files via upload

---
+++ diplom project second/ajax/telegram.php
@@ -0,0 +1,28 @@
+<?php
+$email = $_POST['email'];
+$token = "5239948205:AAFW2kcf-AEXYLVVzuhxTNOToJYns_vURxE";
+$chat_id = "-666001746";
+$arr = array(
+  'User email: ' => $email,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."";
+};
+
+$website="https://api.telegram.org/bot".$token;
+  $params=[
+      'chat_id'=>$chat_id, 
+      'text'=> $txt,
+  ];
+  $ch = curl_init($website . '/sendMessage');
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  echo $result;
+
+?>
--- diplom project second/script/modal.js
+++ diplom project second/script/modal.js
@@ -11,3 +11,49 @@ $('.close').on('click', function(){
 $('#modal-button').on('click', function(){
     $('.modal-window').fadeIn();
 })
+
+$('.modal-form').on('click', function(e){
+    if($(e.target).closest('.modal-second').length ==0){
+        $(this).fadeOut()
+    }
+})
+
+$('.close').on('click', function(){
+    $(this).parents('.modal-form').fadeOut();
+})
+
+$('#button').on('click', function(){
+    $('.modal-form').fadeIn();
+})
+
+
+
+$('#button-second').on('click', function(e){
+    e.preventDefault();
+    let email = $('#email').val().trim();
+
+    if(email == ""){
+        $('#error-email').text('Enter your email');
+        return false
+    }
+
+    $.ajax({
+        url: 'ajax/telegram.php',
+        type: 'POST',
+        cache: false,
+        data: {'email': email}, 
+        dataType: 'html',
+     
+        beforeSend: function(){
+            $('#button-second').prop('disabled', true)
+        },
+
+        success: function(){
+
+            $('.modal-form').fadeIn();
+            $('#email').val('');
+            $('#error-email').text('');
+            $('#button-second').prop('disabled', false)
+        }
+    })
+})
